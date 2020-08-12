738712d033e68146ef3f748970dc518796142e6d
evanhongo <evanhongo@gmail.com>
2020-08-12 22:03:21 +0800

    Updates

---
+++ precache-manifest.1da7756a87775c2fa07fada0c1ae8130.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba941d6f63c0eb66520f4db286aec905",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a20bd28319b69ecf153",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "4bbf18a829b903d6fc3b",
+    "url": "/static/js/2.b7c33195.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b7c33195.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a20bd28319b69ecf153",
+    "url": "/static/js/main.c02c07e8.chunk.js"
+  },
+  {
+    "revision": "541fa85fe4d2dc2f41cf",
+    "url": "/static/js/runtime-main.4e25aefe.js"
+  }
+]);
